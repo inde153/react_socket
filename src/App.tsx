21af30601effadfa830fcdfb4f559fd2bad8336b
inde153 <inde153@naver.com>
2024-03-14 03:41:14 +0900

    Feat : Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+const paths = [{ path: '/', element: Home }];
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {paths.map((ele) => (
+            <Route key={ele.path} path={ele.path} element={<ele.element />} />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
